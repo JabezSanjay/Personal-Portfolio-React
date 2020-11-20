cf61346220953a367b2d5c726c7f205ee26f0921
Jabez Sanjay <jbzsanjay2000@gmail.com>
2020-11-20 17:22:47 +0530

    Routing and file structure completed

---
+++ src/Routes.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Projects from "./pages/Projects/Projects";
+import Contact from "./pages/Contact/Contact";
+
+const Routes = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" exact component={About} />
+        <Route path="/projects" exact component={Projects} />
+        <Route path="/contact" exact component={Contact} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default Routes;
