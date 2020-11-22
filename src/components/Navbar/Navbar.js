551ea30995975cfc1bbab06281abd151850338ca
Jabez Sanjay <jbzsanjay2000@gmail.com>
2020-11-22 17:42:24 +0530

    Added new components.

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Navbar.modules.scss";
+
+const Navbar = () => {
+  return <div>Navbar</div>;
+};
+
+export default Navbar;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,13 @@
 import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
 import "./Home.modules.scss";
 
 const Home = () => {
-  return <h1>Home</h1>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 };
 
 export default Home;
