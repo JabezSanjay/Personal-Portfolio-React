a68da38688b908c274ce1e9ad62f681ad0a7c240
Jabez Sanjay <jbzsanjay2000@gmail.com>
2020-11-23 19:17:02 +0530

    Minor changes in Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,46 +1,30 @@
-import React, { useState } from "react";
-import { withRouter, Link } from "react-router-dom";
+import React from "react";
 import "./Navbar.modules.scss";
 
-const Navbar = ({ history }) => {
-  const [navbar, setNavbar] = useState(false);
-
-  const navbarClicked = () => {
-    setNavbar(!navbar);
-  };
-  const navbarComponent = () => {
-    return (
-      <header className="header">
-        <nav className="navbar">
-          <h1 className="navbar__logo">T-Shirts</h1>
-          <ul
-            className={
-              navbar ? "navbar__links navbar--active" : "navbar__links"
-            }
-          >
-            <li>
-              <Link to="/" onClick={navbarClicked}>
-                Home
-              </Link>
-            </li>
-
-            <li>
-              <Link to="/cart" onClick={navbarClicked}>
-                Cart
-              </Link>
-            </li>
-          </ul>
-          <div
-            className={navbar ? "hamburger-menu bar-change" : "hamburger-menu"}
-            onClick={navbarClicked}
-          >
-            <div className="bar1">-----</div>
-          </div>
-        </nav>
-      </header>
-    );
-  };
-  return <div>{navbarComponent()}</div>;
+const navbar = () => {
+  const logoLetters = [
+    "J",
+    "A",
+    "B",
+    "E",
+    "",
+    "Z",
+    "S",
+    "A",
+    "N",
+    "J",
+    "A",
+    "Y",
+  ];
+  return (
+    <div>
+      <div className="wavy">
+        {logoLetters.map((letter, index) => {
+          return <span>{letter}</span>;
+        })}
+      </div>
+    </div>
+  );
 };
 
-export default withRouter(Navbar);
+export default navbar;
