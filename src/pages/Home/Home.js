d17f6bcd31395b7d8912890f9f4893ee94eaca41
Jabez Sanjay <jbzsanjay2000@gmail.com>
2020-11-26 18:27:10 +0530

    Completed mobile view of Homepage

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -7,6 +7,15 @@ import "./Home.modules.scss";
 const Home = () => {
   return (
     <div className="homepage">
+      <div className="mainhead">
+        <h1 id="main-text">
+          <span>I </span> bridge the gap between <span>you</span> and your
+          <span> customer</span> using <span>code</span>!
+        </h1>
+        <h1 id="color-dance-text">
+          <span>FullStack </span>Web Developer
+        </h1>
+      </div>
       <Navbar />
       <SocialMedia />
       <WaterFlowButton />
