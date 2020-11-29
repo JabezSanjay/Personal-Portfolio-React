923867e936eca790e3b9caf42dee95b72e5ea917
Jabez Sanjay <jbzsanjay2000@gmail.com>
2020-11-29 09:15:44 +0530

    Minor changes in the homepage.

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return <div className="aboutpage"></div>;
 };
 
 export default About;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,11 +2,12 @@ import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import SocialMedia from "../../components/SocialMedia/SocialMedia";
 import WaterFlowButton from "../../components/Buttons/WaterFlowButton";
+import About from "../About/About";
 import "./Home.modules.scss";
 
 const Home = () => {
-  return (
-    <div className="homepage">
+  const homepageContent = () => {
+    return (
       <div className="mainhead">
         <h1 id="main-text">
           <span>I </span> bridge the gap between <span>you</span> and your
@@ -16,9 +17,19 @@ const Home = () => {
           <span>FullStack </span>Web Developer
         </h1>
       </div>
-      <Navbar />
-      <SocialMedia />
-      <WaterFlowButton />
+    );
+  };
+  return (
+    <div>
+      <section className="homepage">
+        {homepageContent()}
+        <Navbar />
+        <SocialMedia />
+        <WaterFlowButton />
+      </section>
+      <section>
+        <About />
+      </section>
     </div>
   );
 };
