2cd99e7aed79f9e1bf949ea1e6af7d975a0925b4
Jabez Sanjay <jbzsanjay2000@gmail.com>
2020-12-02 00:41:13 +0530

    Added contact form

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,8 +1,62 @@
 import React from "react";
 import "./Contact.modules.scss";
+import SocialMedia from "../../components/SocialMedia/SocialMedia";
 
 const Contact = () => {
-  return <div className="contactPage">Contact</div>;
+  return (
+    <section className="contactPage">
+      <div className="container">
+        <h2>Contact Me</h2>
+        <div className="row100">
+          <div className="col">
+            <div className="inputBox">
+              <input type="text" name="" required="required" />
+              <span className="contact-text">First Name</span>
+              <span className="contact-line"></span>
+            </div>
+          </div>
+          <div className="col">
+            <div className="inputBox">
+              <input type="text" name="" required="required" />
+              <span className="contact-text">Last Name</span>
+              <span className="contact-line"></span>
+            </div>
+          </div>
+        </div>
+        <div className="row100">
+          <div className="col">
+            <div className="inputBox">
+              <input type="text" name="" required="required" />
+              <span className="contact-text">Email</span>
+              <span className="contact-line"></span>
+            </div>
+          </div>
+          <div className="col">
+            <div className="inputBox">
+              <input type="text" name="" required="required" />
+              <span className="contact-text">Mobile Number</span>
+              <span className="contact-line"></span>
+            </div>
+          </div>
+          <div className="row100">
+            <div className="col">
+              <div className="inputBox textarea">
+                <textarea required="required"></textarea>
+                <span className="contact-text">Type your Message here!</span>
+                <span className="contact-line"></span>
+              </div>
+            </div>
+          </div>
+          <div className="row100">
+            <div className="col">
+              <input type="submit" value="Send" />
+            </div>
+          </div>
+        </div>
+        <SocialMedia />
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
