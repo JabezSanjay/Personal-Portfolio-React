7233572faf0894f47db3a51d3a3bd735957a43f6
Jabez Sanjay <jbzsanjay2000@gmail.com>
2020-12-02 18:35:14 +0530

    Added basic image structure in about

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,8 +1,29 @@
 import React from "react";
 import "./About.modules.scss";
+import Express from "../../images/express.svg";
+import MongoDB from "../../images/mongodb.svg";
+import ReactJs from "../../images/react.svg";
+import NodeJs from "../../images/nodejs.svg";
+import Sass from "../../images/sass.svg";
+import Git from "../../images/git.svg";
 
 const About = () => {
-  return <div className="aboutPage">About</div>;
+  return (
+    <div className="aboutPage">
+      <div className="aboutPage__letters">
+        <h2 className="aboutPage__head">My Expertise</h2>
+        <p className="aboutPage__para">My skills mean a world to me</p>
+      </div>
+      <div className="aboutPage__images">
+        <img src={MongoDB} alt="" />
+        <img src={Express} alt="" />
+        <img src={ReactJs} alt="" />
+        <img src={NodeJs} alt="" />
+        <img src={Sass} alt="" />
+        <img src={Git} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default About;
