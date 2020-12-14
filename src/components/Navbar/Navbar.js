70ef79b9b9199dfc0029350665a0e13ca4d34ad8
Jabez Sanjay <jbzsanjay2000@gmail.com>
2020-12-14 12:56:14 +0530

    Deleted the animation in navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,17 +5,7 @@ const navbar = () => {
   return (
     <div className="navbar">
       <div className="navbar__logo">
-        <span>J</span>
-        <span>A</span>
-        <span>B</span>
-        <span>E</span>
-        <span>Z</span>
-        <span>S</span>
-        <span>A</span>
-        <span>N</span>
-        <span>J</span>
-        <span>A</span>
-        <span>Y</span>
+        <span>Jabez Sanjay</span>
       </div>
     </div>
   );
--- src/components/SocialMedia/SocialMedia.js
+++ src/components/SocialMedia/SocialMedia.js
@@ -1,25 +1,32 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import "./SocialMedia.scss";
+import { ExternalLink } from "react-external-link";
 
 const SocialMedia = () => {
   return (
     <div className="socialmedia">
       <ul>
         <li>
-          <a href="/">
-            <i className="fa fa-github" aria-hidden="true"></i>
-          </a>
+          <ExternalLink href="https://github.com/JabezSanjay">
+            <a>
+              <i className="fa fa-github" aria-hidden="true"></i>
+            </a>
+          </ExternalLink>
         </li>
         <li>
-          <a href="/">
-            <i className="fa fa-youtube" aria-hidden="true"></i>
-          </a>
+          <ExternalLink href="https://www.youtube.com/c/jabezvlogs">
+            <a>
+              <i className="fa fa-youtube" aria-hidden="true"></i>
+            </a>
+          </ExternalLink>
         </li>
         <li>
-          {" "}
-          <a href="/">
-            <i className="fa fa-linkedin" aria-hidden="true"></i>
-          </a>
+          <ExternalLink href="https://www.linkedin.com/in/jabezsanjay/">
+            <a>
+              <i className="fa fa-linkedin" aria-hidden="true"></i>
+            </a>
+          </ExternalLink>
         </li>
       </ul>
     </div>
