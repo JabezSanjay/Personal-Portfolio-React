111fcd3a825c005435ff85d5e238cff9a45fd0f6
Jabez Sanjay <jbzsanjay2000@gmail.com>
2021-03-13 22:29:39 +0530

    Added colors.js file

---
+++ src/assets/colors.js
@@ -0,0 +1,14 @@
+const COLORS = {
+  primaryBackgroundColor: "rgb(80, 133, 247)",
+  secondaryBackgroundColor: "rgb(1, 37, 116)",
+  whiteColor: "#fff",
+  blackColor: "#000",
+  black: "#000000",
+  blueRibbon: "#0069ec",
+  dodgerBlue: "#25a3ff",
+  mineShaft: "#333333",
+  BUTTON_BORDER: "#acb4c1",
+  white: "#ffffff",
+};
+
+export default COLORS;
