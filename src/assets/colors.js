3ef565aed02c13d13c9c3b0f1c5f8ee1a9102111
Jabez Sanjay <jbzsanjay2000@gmail.com>
2021-03-13 22:30:39 +0530

    Added colors constants to colors.js

--- src/assets/colors.js
+++ src/assets/colors.js
@@ -1,14 +1,8 @@
 const COLORS = {
-  primaryBackgroundColor: "rgb(80, 133, 247)",
-  secondaryBackgroundColor: "rgb(1, 37, 116)",
+  primaryColor: "#45f3ff",
+  secondaryColor: "#008aff",
   whiteColor: "#fff",
-  blackColor: "#000",
-  black: "#000000",
-  blueRibbon: "#0069ec",
-  dodgerBlue: "#25a3ff",
-  mineShaft: "#333333",
-  BUTTON_BORDER: "#acb4c1",
-  white: "#ffffff",
+  tertiaryColor: "#222831",
 };
 
 export default COLORS;
